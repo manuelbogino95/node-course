2738d8ee7a9b3d068f77099e0b72fe6b1e614a9f
Manuel Bogino <manuelbogino@gmail.com>
2019-10-21 23:23:37 -0300

    Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
-const fs = require('fs') 
+const getNotes = require('./notes.js') 
 
-// fs.writeFileSync('notes.txt', 'My name is Manuel.')
+const msg = getNotes()
 
-fs.appendFileSync('notes.txt', ' I live in Rosario.')
+console.log(msg)
