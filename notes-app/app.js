b626f27f0960f9b2b2d119dccb9cb5ea83fc8c24
Manuel Bogino <manuelbogino@gmail.com>
2019-10-22 20:48:42 -0300

    Adding and Removing notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,5 @@
-const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 // Customize yargs version
 yargs.version('1.1.0')
@@ -22,24 +21,30 @@ yargs.command({
 		}
 	},
 	handler: function (argv) {
-		console.log('Title: ', argv.title)
-		console.log('Body: ', argv.body)
+		notes.addNote(argv.title, argv.body)
 	}
 })
 
 // Create remove command
 yargs.command({
 	'command': 'remove',
-	describe: 'Remove a note',
-	handler: function () {
-		console.log('Removing the note!')
+	'describe': 'Remove a note',
+	'builder': {
+		'title': {
+			'describe': 'Note title',
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		notes.removeNote(argv.title)
 	}
 })
 
 // Create list command
 yargs.command({
 	'command': 'list',
-	describe: 'List your notes',
+	'describe': 'List your notes',
 	handler: function () {
 		console.log('Listing out all notes')
 	}
@@ -48,7 +53,7 @@ yargs.command({
 // Create read command
 yargs.command({
 	'command': 'read',
-	describe: 'Read a note',
+	'describe': 'Read a note',
 	handler: function () {
 		console.log('Reading a note')
 	}
