40ba2251ef2b8d39121de72b2c15f0bcea5ab9b0
Manuel Bogino <manuelbogino@gmail.com>
2020-03-03 19:01:19 -0300

    Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -5,15 +5,59 @@ const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-  if(error) {
+  if (error) {
     console.log('Unable to connect to database!')
   }
 
   const db = client.db(databaseName)
 
 
-  db.collection('users').insertOne({
-    name: 'Manuel',
-    age: 24
+  // db.collection('users').insertOne({
+  //   name: 'Manuel',
+  //   age: 24
+  // }, (error, result) => {
+  //   if (error) {
+  //     console.log('Unable to insert user!')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Maitena',
+  //     age: 24
+  //   },
+  //   {
+  //     name: 'Clara',
+  //     age: 4
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     console.log('Unable to insert documents!')
+  //   }
+
+  //   console.log(result.ops)
+  // })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Task 1',
+      completed: true
+    },
+    {
+      description: 'Task 2',
+      completed: false
+    },
+    {
+      description: 'Task 3',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) {
+      console.log('Unable to insert documents')
+    }
+
+    console.log(result.ops)
   })
 })
