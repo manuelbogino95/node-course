88b4901ca3420d15abb5860bb7fc93c765b1cb90
Manuel Bogino <manuelbogino@gmail.com>
2020-03-08 21:16:31 -0300

    Adding Images to User Profile

--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -4,7 +4,6 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 const upload = multer({ 
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -95,10 +94,33 @@ router.delete('/users/me', auth, async (req, res) => {
   }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save()
   res.send()
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+
+    if (!user || !user.avatar) {
+      throw new Error()
+    }
+
+    res.set('Content-Type', 'image/jpg')
+    res.send(user.avatar)
+  } catch (error) {
+    res.status(404).send(error)
+  }
 })
 
 module.exports = router
